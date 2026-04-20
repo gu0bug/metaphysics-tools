b823461ac3747559a027630094a2011723bfd164
unknown <zhanggggfd@gmail.com>
2026-04-20 21:54:21 +0800

    feat: make GTM ID configurable via config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+window.CONFIG = {
+    // Google Tag Manager ID
+    GTM_ID: 'GTM-PNLNGK78'
+};
---
+++ tracking.js
@@ -0,0 +1,10 @@
+(function() {
+    const gtmId = window.CONFIG && window.CONFIG.GTM_ID;
+    if (!gtmId) return;
+
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer', gtmId);
+})();
